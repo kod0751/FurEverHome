cdc1012949b6826c8082c5e694d2228d7cc935b5
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-10-06 13:51:27 +0900

    Category 기능

--- src/pages/PetList.jsx
+++ src/pages/PetList.jsx
@@ -32,10 +32,6 @@ export default function PetListpage() {
     neut: '전체',
   });
 
-  const handleTextClick = () => {
-    console.log(filteredData);
-  };
-
   useEffect(() => {
     const fetchData = async () => {
       // const KEY = process.env.REACT_APP_KEY;
@@ -51,16 +47,38 @@ export default function PetListpage() {
 
   useEffect(() => {
     const newFilteredData = items.filter((item) => {
+      const currentYear = new Date().getFullYear();
+      const birthYear = item.AGE_INFO.slice(0, 4);
+      const petAge = currentYear - birthYear;
+
       return (
         (filters.region === '시도군' || item.SIGUN_NM === filters.region) &&
-        (filters.state === '전체' || item.STATE_NM === filters.state) &&
-        (filters.kind === '전체' || item.SPECIES_NM === filters.kind) &&
-        (filters.age === '전체' || item.age === filters.age) &&
-        (filters.gender === '전체' || item.SEX_NM === filters.gender) &&
-        (filters.neut === '전체' || item.NEUT_YN === filters.neut)
+        (filters.state === '전체' ||
+          (filters.state === '보호중' && item.STATE_NM.includes('보호중')) ||
+          (filters.state === '종료' && item.STATE_NM.includes('종료'))) &&
+        (filters.kind === '전체' ||
+          (filters.kind === '강아지' && item.SPECIES_NM.includes('개')) ||
+          (filters.kind === '고양이' && item.SPECIES_NM.includes('고양이')) ||
+          (filters.kind === '고양이' &&
+            item.SPECIES_NM.includes('기타축종'))) &&
+        (filters.age === '전체' ||
+          (filters.age === '1세미만' && petAge === 0) ||
+          (filters.age === '1살~5살' && petAge >= 1 && petAge <= 4) ||
+          (filters.age === '6살~9살' && petAge >= 5 && petAge <= 8) ||
+          (filters.age === '10살이상' && petAge >= 9)) &&
+        (filters.gender === '전체' ||
+          (filters.gender === '여아'
+            ? item.SEX_NM === 'F'
+            : item.SEX_NM === 'M')) &&
+        (filters.neut === '전체' ||
+          (filters.neut === '완료' && item.NEUT_YN === 'Y') ||
+          (filters.neut === '미완료' && item.NEUT_YN === 'N') ||
+          (filters.neut === '알수없음' && item.NEUT_YN === 'U'))
       );
     });
+
     setFilteredData(newFilteredData); // 필터링된 데이터를 업데이트
+    setPage(1); // 필터링을 하면 첫페이지로
   }, [filters, items]);
 
   return (
@@ -71,15 +89,15 @@ export default function PetListpage() {
         <CategorySection filters={filters} setFilters={setFilters} />
         <TextArea>
           <span className="highlight" onClick={() => handleTextClick()}>
-            {items.length}
+            {filteredData.length}
           </span>
           마리의 친구들이 기다리고 있어요
         </TextArea>
-        <PetlistSection data={items.slice((page - 1) * 15, page * 15)} />
+        <PetlistSection data={filteredData.slice((page - 1) * 15, page * 15)} />
         <PageNation
           value={page}
           onChange={setPage}
-          total={items.length / 15 + 1}
+          total={filteredData.length / 15 + 1}
         />
       </ThemeProvider>
     </>
