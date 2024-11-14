390ffed22c9b5bf8f1298e84ec72b28ba6e1e2d0
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-11-14 18:42:12 +0900

    fix: 키값 변경

--- src/api/oneDayPet.js
+++ src/api/oneDayPet.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // 스와이퍼 아이템 가져오기 함수
 export const fetchSwiperItems = async (tomorrowDate) => {
+  const KEY = import.meta.env.VITE_REACT_APP_KEY;
   const { data } = await axios.get(
-    `https://openapi.gg.go.kr/AbdmAnimalProtect?KEY=e852a9e19dbf4ef291979109612f0b27&PBLANC_END_DE=${tomorrowDate}&Type=json&pSize=20`
+    `https://openapi.gg.go.kr/AbdmAnimalProtect?KEY=${KEY}&PBLANC_END_DE=${tomorrowDate}&Type=json&pSize=20`
   );
   return data.AbdmAnimalProtect[1].row;
 };
