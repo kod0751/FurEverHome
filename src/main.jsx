ed0cdb80d6f7737d8bf10531946f48bdbe4a35b7
kod0751 <50727457+kod0751@users.noreply.github.com>
2024-11-16 23:30:55 +0900

    style: ThemeProvider 위치수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </QueryClientProvider>
 );
